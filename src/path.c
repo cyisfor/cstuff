c8b5dfdadd65ca7b91aff039979fd7c0a6acff8f
user <nobody>
2016-09-21 06:13:41 +0000

    initial import

---
+++ src/path.c
@@ -0,0 +1,182 @@
+#include "path.h"
+
+#include <string.h> // glib has no memcpy
+
+typedef struct path {
+	const gchar* base;
+	struct stat info;
+	gsize len;
+	gsize allocated_len;
+	bool constant;
+	bool statted;
+} *path;
+
+path path_new(gchar* s, gsize len) {
+	path self = g_new(struct path,1);
+	self->base = s;
+	self->len = len;
+	self->allocated_len = len;
+	self->constant = false;
+}
+
+#define ORDER 8
+#define BLOCK (1<<ORDER)
+
+static path prepare_append(path self, gsize length) {
+	if(len == 0) return;
+	if(self->constant) {
+		path old = self;
+		self = g_new(struct path,1);
+		self->constant = false;
+		self->len = old->len;
+		self->allocated_len = ((self->len + length) >> ORDER + 1) << ORDER;
+		self->base = g_new(gchar,self->allocated_len);
+		g_memcpy(self->base,old->base,self->len);
+		// don't self->len += length yet
+	} else {
+		if(self->allocated_len < self->len + length) {
+			self->allocated_len = ((self->len + length) >> ORDER + 1) << ORDER;
+			self->base = g_realloc(self->base, self->allocated_len);
+		}
+	}
+	return self;
+}
+
+static void appendslow(path self, const gchar* s, gsize length) {
+	/* since the tail of base is never used, there's never a trailing
+		 suffix. Either the string overlaps from before base, it
+		 totally overlaps, or it doesn't overlap. */
+	if(G_UNLIKELY(s >= self->base)) {
+		if(s < self->base + self->len) {
+			// totally overlapping
+			g_memmove(self->base+self->len,s,length);
+		} else {
+			g_assert(s + length < self->base + self->len);
+			g_memcpy(self->base+self->len,s,length);
+		}
+	} else {
+		if(s + length > self->base) {
+			// overlapping suffix
+			gsize pivot = self->base - s;
+			g_memcpy(self->base+self->len,s,pivot);
+			g_memmove(self->base+self->len+pivot,s+pivot,length-pivot);
+		} else {
+			g_memcpy(self->base+self->len,s,length);
+		}
+	}
+	self->len += len;
+}
+
+// for known consts, like separators and such.
+static void appendfast(path self, const gchar* s, gsize length) {
+	switch(length) {
+	case 1:
+		self->base[self->len++] = s[0];
+		break;
+	case 2:
+		self->base[self->len] = s[0];
+		self->base[self->len+1] = s[1];
+		len += 2;
+		break;
+	case 3:
+		self->base[self->len] = s[0];
+		self->base[self->len+1] = s[1];
+		self->base[self->len+2] = s[2];
+		len += 3;
+		break;
+	default:
+		g_memcpy(self->base+self->len, s, length);
+		self->len += len;
+		break;
+	};
+}
+
+
+path path_add(path self, ...) {
+	g_assert(self != NULL);
+	va_list arg;
+	va_start(arg, self);
+	for(;;) {
+		gchar* next = va_arg(arg, gchar*);
+		if(next == NULL) break;
+		gsize len = g_strlen(next);
+		self = prepare_append(self, len+sizeof(G_DIR_SEPARATOR_S)-1);
+		appendfast(self,G_DIR_SEPARATOR_S,sizeof(G_DIR_SEPARATOR_S)-1);
+		appendslow(self,next,len);
+	}
+	return self;
+}
+
+path path_lookup(GUserDirectory id) {
+	static struct path paths[G_USER_N_DIRECTORIES];
+	static bool gotchas[G_USER_N_DIRECTORIES] = {};
+	if(!gotchas[id]) {
+		const gchar* s = g_get_user_special_dir(id);
+		paths[id].base = s;
+		paths[id].len = strlen(s);
+		paths[id].constant = true;
+		gotchas[id] = true;
+	}
+
+	return &paths[id];
+}
+
+/* we can't use g_stat because it doesn't use timespec modification times */
+
+struct stat path_stat(path self) {
+	if(!self->statted) {
+		self->statted = true;
+		g_assert(0==stat(self->base,&self->info));
+	}
+	return self->info;
+}	
+
+path path_home(void) {
+	static struct path home;
+	bool gotcha = false;
+	if(!gotcha) {
+		const gchar* s = g_getenv("HOME");
+		if(s == NULL) {
+			s = g_get_home_dir();
+		} else {
+			if(g_path_is_absolute(s)  && (0 == g_stat(s,&home.info))) {
+				home.statted = true;
+			} else {
+				// to hell with "undefined behavior"
+				g_warning("HOME is set to something bad %s",s);
+				const gchar* old = s;
+				g_unsetenv("HOME");
+				s = g_get_home_dir();
+				g_setenv("HOME",old);
+			}
+		}
+		home.base = s;
+		home.len = g_strlen(s);
+		home.constant = true;
+		gotcha = true;
+	}
+	return &home;
+}
+
+const gchar* path_base(path self) {
+	return self->base;
+}
+
+gsize path_len(path self) {
+	return self->len;
+}
+
+void path_free(path self) {
+	if(self->constant) return;
+	free(self->base);
+	free(self);
+}
+
+bool left_is_older(path left, path right) {
+	if(left->info.st_mtime < right->info.st_mtime) return true;
+	if(left->info.st_mtime == right->info.st_mtime)
+		if(left->info.st_mtim.tv_nsec < right->info.st_mtim.tv_nsec)
+			return true;
+	return false;
+}
+
---
+++ src/path.h
@@ -0,0 +1,16 @@
+#include <sys/stat.h>
+#include <glib.h>
+
+typedef struct path *path;
+
+// append gchar* components to path
+path path_add(path self, ...);
+
+// look up a base path
+path path_lookup(GUserDirectory id);
+path path_home();
+
+const gchar* path_base(path);
+gsize path_len(path);
+
+void path_free(path);
