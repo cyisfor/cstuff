ae3f03733e5e2d60f88475c85bf172b23a21d7a8
autocommit <autocommit>
2017-10-04 01:35:55 +0000

    auto 0 17031 168723

---
+++ src/mmapfile.c
@@ -0,0 +1,19 @@
+#include "mmapfile.h"
+
+#include <sys/stat.h> // fstat
+#include <fcntl.h> // open
+#include <sys/mman.h> // mmap
+#include <assert.h>
+#include <unistd.h> // close
+
+void* mmapfile(const char* path, size_t* osize) {
+	 int fd = open(path, O_RDONLY);
+	 assert(fd >= 0);
+	 struct stat st;
+	 assert(0 == fstat(fd, &st));
+	 void* b = mmap(NULL, st.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+	 assert(b != MAP_FAILED);
+	 close(fd);
+	 *osize = st.st_size;
+	 return b;
+}
---
+++ src/mmapfile.h
@@ -0,0 +1,2 @@
+#include <stdlib.h> // size_t
+void* mmapfile(const char* path, size_t* osize);
