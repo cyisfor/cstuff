5f25ba5c0c361fa9b1856981e5c4ba3ce360c8e8
autocommit <autocommit>
2017-10-27 22:12:30 +0000

    auto 0 15 207

--- src/defer.h
+++ src/defer.h
@@ -2,6 +2,8 @@
 #define symjoin2(a,b) a ## b
 #define symjoin(a,b) symjoin2(a,b)
 
-#define DEFER auto void symjoin(cleanupfunc,__LINE__)(void* nada); \
-	char __attribute__((__cleanup__(symjoin(cleanupfunc,__LINE__)))) symjoin(defervar,__LINE__); \
-	void symjoin(cleanupfunc,__LINE__)(void* nada)
+#define DEFERNAME(name,var) auto void name(void* nada); \
+	char __attribute__((__cleanup__(name))) var; \
+	void name(void* nada)
+
+#define DEFER DEFERNAME(symjoin(cleanupfunc,__LINE__),symjoin(defervar,__LINE__))
