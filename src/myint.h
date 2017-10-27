60a7cc868ecfe70a105e2b97c11745f04d7e8fb3
autocommit <autocommit>
2017-10-27 19:27:16 +0000

    auto 0 49 363

---
+++ src/myint.h
@@ -0,0 +1,17 @@
+#ifndef _MYINT_H_
+#define _MYINT_H_
+
+#include <stdint.h> // meh
+
+typedef uint8_t u8;
+typedef int8_t s8;
+typedef uint16_t u16;
+typedef int16_t s16;
+typedef uint32_t u32;
+typedef int32_t s32;
+typedef uint64_t u64;
+typedef int64_t s64;
+
+typedef s64 identifier;
+
+#endif /* _MYINT_H_ */
