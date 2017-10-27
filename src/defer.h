36a15feafbbefffb7f75ed9ed1369d3d30e8bbf2
autocommit <autocommit>
2017-10-27 19:03:53 +0000

    auto 0 20 289

--- src/defer.h
+++ src/defer.h
@@ -1,6 +1,7 @@
 // CPP sux
-#define symjoin(a,b) a ## b
+#define symjoin2(a,b) a ## b
+#define symjoin(a,b) symjoin2(a,b)
 
-#define DEFER() void symjoin(cleanupfunc,__LINE__)(void* nada); \
-	char __attribute__((__cleanup__(symjoin(cleanupfunc,__LINE__)))) \
+#define DEFER auto void symjoin(cleanupfunc,__LINE__)(void* nada); \
+	char __attribute__((__cleanup__(symjoin(cleanupfunc,__LINE__)))) symjoin(defervar,__LINE__); \
 	void symjoin(cleanupfunc,__LINE__)(void* nada)
