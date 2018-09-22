2b53c0c24ca88a63a283338c3334b484ae4f8983
autocommit <autocommit>
2018-09-22 23:39:03 +0000

    auto 0 19 231

--- status.mk
+++ status.mk
@@ -1,9 +1,11 @@
 $(file > make-status.log)
-status=$(info $(statusPrefix)$(1)$(2)$(3)$(statusSuffix))$(file >> make-status.log, $(1) $(2) $(3))
+status=$(info $(statusPrefix)STATUS$(statusMidfix)$(1)$(2)$(3)$(statusSuffix))$(file >> make-status.log, $(1) $(2) $(3))
 # https://en.wikipedia.org/wiki/ANSI_escape_code#Colors
-statusColor=36
+statusColor=1
+statusMidColor=36
 # cyan
 statusPrefix=$(shell echo -e '\x1b[$(statusColor)m')
+statusMidfix=$(shell echo -e '\x1b[$(statusMidColor)m')
 statusSuffix=$(shell echo -e '\x1b[0m')
 
 test:
