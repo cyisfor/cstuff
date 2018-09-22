3196d85fe73e4bba3e30de717f85d1be0ec2e55f
user <email>
2018-09-22 23:32:20 +0000

    colored terminal stuff

--- status.mk
+++ status.mk
@@ -1,2 +1,10 @@
 $(file > make-status.log)
-status=$$(file >> make-status.log, $(1) $(2) $(3))
+status=$(info $(statusPrefix)$(1)$(2)$(3)$(statusSuffix))$(file >> make-status.log, $(1) $(2) $(3))
+# https://en.wikipedia.org/wiki/ANSI_escape_code#Colors
+statusColor=36
+# cyan
+statusPrefix=$(shell echo -e '\x1b[$(statusColor)m')
+statusSuffix=$(shell echo -e '\x1b[0m')
+
+test:
+	$(call status, test this thing)
