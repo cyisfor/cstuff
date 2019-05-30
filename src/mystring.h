742b8ec00923d80d145f1269975bd802364ff114
user <email>
2019-05-30 16:17:23 +0000

    .l is STUPID

--- src/mystring.h
+++ src/mystring.h
@@ -9,38 +9,39 @@
 #define LITSIZ(a) (sizeof(a)-1)
 #define LITLEN(a) a, LITSIZ(a)
 
-/* C makes this almost unusable. You cannot create a mutable structure that points
-	 to const data. */
+/* C makes this literally unusable. You cannot create a mutable structure that points
+	 to const data. 
 typedef struct cstring {
 	const char* const s;
 	const size_t l;
 } cstring;
+*/
 
 typedef struct string {
-	const char* s;
-	size_t l;
+	const char* base;
+	size_t len;
 } string;
 
 typedef struct bstring {
-	char* s;
-	size_t l;
+	char* base;
+	size_t len;
 	size_t space;
 } bstring;
 
 
 #define CSTRING(str) (*((const string*)&str)) // any kind of string
 #define STRING(str) (*((string*)&str)) // any kind of string, but may segfault
-#define LITSTR(lit) (string){.s = lit, .l = LITSIZ(lit)}
+#define LITSTR(lit) (string){.base = lit, .len = LITSIZ(lit)}
 static
-bstring bstringstr(const char* s, size_t n) {
-	char* buf = malloc(n);
-	memcpy(buf,s,n);
-	return ((bstring) { .s = buf, .l = n, .space = n });
+bstring bstringstr(const char* str, size_t len) {
+	char* buf = malloc(len);
+	memcpy(buf,str,len);
+	return ((bstring) { .base = buf, .len = len, .space = len });
 }
 
-static string strlenstr(const char* s) {
-	size_t n = strlen(s);
-	return (string) { .s = s, .l = n};
+static string strlenstr(const char* str) {
+	size_t len = strlen(str);
+	return (string) { .base = str, .len = len};
 }
 
 #define bstringlit(lit) bstringstr(LITLEN(lit))
@@ -52,21 +53,21 @@ void strgrow(bstring* st, size_t newmin) {
 	st->space = (st->space * 3)>>1;
 	if(st->space < newmin)
 		st->space = newmin;
-	st->s = realloc(st->s, st->space);
+	st->base = realloc(st->base, st->space);
 }
 
 static
 void strreserve(bstring* st, size_t n) {
 	// could say st.space = pow(1.5,log(st.l+n)/(log(3)-log(2))+1)
 	// but that seems like a lot of expensive math calls, when the loop'll only happen like 4 times max
-	while(st->l + n > st->space) strgrow(st,st->l+n);
+	while(st->len + n > st->space) strgrow(st,st->len+n);
 }
 
 static
 void straddn(bstring* st, const char* c, size_t n) {
 	strreserve(st, n);
-	memcpy(st->s + st->l, c, n);
-	st->l += n;
+	memcpy(st->base + st->len, c, n);
+	st->len += n;
 }
 
 #define stradd(st,lit) straddn(st,lit,sizeof(lit)-1)
@@ -74,24 +75,24 @@ void straddn(bstring* st, const char* c, size_t n) {
 static
 void straddint(bstring* st, int i) {
 	strreserve(st,0x10);
-	st->l += snprintf(st->s + st->l, 0x10, "%x",i);
+	st->len += snprintf(st->base + st->len, 0x10, "%x",i);
 }
 
 static
 void strrewind(bstring* st) {
-	st->l = 0;
+	st->len = 0;
 }
 
 static
 void strclear(bstring* st) {
-	// can cheat, by setting ->s to static and ->space to 0
+	// can cheat, by setting ->base to static and ->space to 0
 	if(st->space == 0) return;
-	free(st->s);
-	st->s = NULL;
-	st->l = 0;
+	free(st->base);
+	st->base = NULL;
+	st->len = 0;
 	st->space = 0;
 }
 
-#define STRANDLEN(st) st.s, st.l
+#define STRANDLEN(st) st.base, st.len
 
 #endif /* _MYSTRING_H_ */
