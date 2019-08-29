007a7908142ec468c745634dfa83fe8b63587fcf
autocommit <autocommit>
2019-08-29 06:54:37 +0000

    auto 0 53 624

--- db_types.snippet.h
+++ db_types.snippet.h
@@ -2,19 +2,31 @@
 #error wha
 #endif
 
+/* cpp sucks... */
+#define BIND_NAME(TYPE) CONCATSYM(db_bind_, TYPE)
+#define COL_NAME(TYPE) CONCATSYM(db_column_, TYPE)
+
 #ifdef IMPLEMENTATION
+#define SQLITE_BIND_NAME(TYPE) CONCATSYM(sqlite3_bind_, TYPE)
+#define SQLITE_COL_NAME(TYPE) CONCATSYM(sqlite3_column_, TYPE)
+
 EXPORT
-int CONCATSYM(db_bind_, TYPE)(db_stmt stmt, int col, BIND_ARGS) {
-	return CONCATSYM(sqlite3_bind_, TYPE)(stmt->sqlite, col, BIND_PARAMS);
+int BIND_NAME(TYPE)(db_stmt stmt, int col, BIND_ARGS) {
+	return SQLITE_BIND_NAME(TYPE)(stmt->sqlite, col, BIND_PARAMS);
 }
 EXPORT
-COLUMN_RETURN CONCATSYM(db_column_, TYPE)(db_stmt stmt, int col) {
-	return CONCATSYM(sqlite3_column_, TYPE)(stmt, col);
+COLUMN_RETURN COL_NAME(TYPE)(db_stmt stmt, int col) {
+	return SQLITE_COL_NAME(TYPE)(stmt, col);
 }
+#undef SQLITE_BIND_NAME
+#undef SQLITE_COL_NAME
+
 #else  /* IMPLEMENTATION */
 /* interface... */
-int CONCATSYM(db_bind_, TYPE)(db_stmt, int col, BIND_ARGS);
-COLUMN_RETURN CONCATSYM(db_column, TYPE)(db_stmt, int col);
+int BIND_NAME(TYPE)(db_stmt, int col, BIND_ARGS);
+COLUMN_RETURN COL_NAME(TYPE)(db_stmt, int col);
 #endif
 #undef TYPE
+#undef BIND_NAME
+#undef COL_NAME
 /* leave the rest for db_all_types.snippet.h to redefine */
