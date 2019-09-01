b4ec8ba5c88662e58108004d0db3cbbc21f69f0c
autocommit <autocommit>
2019-09-01 20:59:36 +0000

    auto 0 12 210

--- record.h
+++ record.h
@@ -8,7 +8,7 @@ enum level {
 	DEBUG
 };
 
-void record_init(void); 		/* TODO: customize this during runtime? */
+void record_init(void); 		/* TODO: customize this w/out getenv? */
 
 struct record_params {
 	enum level level;
@@ -18,6 +18,14 @@ struct record_params {
 };
 
 void record_f(struct record_params, const char* fmt, ...);
-#define record(level, fmt, ...) ({ struct record_params params = {level, __FILE__, sizeof(__FILE__)-1, __LINE__}; record_f(params, fmt, ## __VA_ARGS__); })
+#define record(level, fmt, ...) ({					\
+			struct record_params params = {			\
+				level,								\
+				__FILE__,							\
+				sizeof(__FILE__)-1,					\
+				__LINE__							\
+			};										\
+			record_f(params, fmt, ## __VA_ARGS__);	\
+		})
 
 #endif /* RECORD_H */
