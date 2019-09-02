f50a3a8865acc4abbe5a9156601de36427dd02ed
autocommit <autocommit>
2019-09-02 03:42:07 +0000

    auto 0 46 457

--- test/test_basedb.c
+++ test/test_basedb.c
@@ -4,7 +4,7 @@ int main(int argc, char *argv[])
 {
 	basedb db = basedb_open(.path = ":memory:");
 	int res = basedb_exec(db, "CREATE TABLE foo (bar INTEGER)");
-	ensure_eq(res, BASEDB_OK);
+	ensure_eq(res, succeed);
 
 	printf("has table? %s\n", basedb_table(db, "foo") ? "ye" : "ne");
 	
