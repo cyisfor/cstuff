a400d6221249e7a3bf4804b29d6c1b96da323fb8
autocommit <autocommit>
2019-09-02 05:02:16 +0000

    auto 0 28 234

--- itoa.h
+++ itoa.h
@@ -1,5 +1,7 @@
 #include <stdbool.h>
 #include <stdlib.h> // size_t
 
+#define BASE_Q -2
+
 size_t int_to_base(char s[], size_t space, unsigned int n, int base);
 size_t itoa(char s[], size_t space, unsigned int n);
--- test/test_itoa.c
+++ test/test_itoa.c
@@ -3,6 +3,15 @@
 int main(int argc, char *argv[])
 {
     char buf[0x10];
-	size_t amt = itoa(buf, 0x10, 123456)
+	size_t amt = itoa(buf, 0x10, 123456);
+	if(amt != 6) return 1;
+	if(0 != memcmp(buf, LITLEN("123456"))) {
+		return 2;
+	}
+	amt = int_to_base(buf, 0x10, 22, BASE_Q);
+	if(amt != 2) return 3;
+	if(0 != memcmp(buf, LITLEN("BS"))) return 4;
+
+	amt = int_to_base(buf,0x10,0x
     return 0;
 }
