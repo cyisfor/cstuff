3eae085fc34d1b17f8a3f42c04e4188450e6f2e3
autocommit <autocommit>
2019-09-02 04:35:10 +0000

    auto 0 40 307

--- test/test_aton.c
+++ test/test_aton.c
@@ -1,9 +1,18 @@
 #include "aton.h"
+#include "mystring.h"
 int main(int argc, char *argv[])
 {
 	size_t end = 0;
-	long int res = strtol(LITSTR("42"), &end, 10);
-	printf("%ld %ld\n", res, end);
+	long int answer = strtol(LITSTR("42"), &end, 10);
+	if(end != 2) return 1;
+	if(answer != 42) return 2;
+	answer = strtol(LITSTR("42"), &end, 13);
+	if(end != 2) return 3;
+	if(answer != 9*6) return 4;
+
+	answer = strtol(LITSTR("BD"), &end, BASE_Q);
+	if(end != 2) return 5;
+	if(answer != 23) return 6;
     
     return 0;
 }
