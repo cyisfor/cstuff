1de08e739790e6f7b744c52af94a8d0b84502bb0
user <email>
2019-09-02 04:06:39 +0000

    basedb works

--- test/test_basedb.c
+++ test/test_basedb.c
@@ -3,9 +3,9 @@
 int main(int argc, char *argv[])
 {
 	record_init();
-	basedb db = basedb_open(.path = "/tmp/derp.sqlite");
+	basedb db = basedb_open(.path = ":memory:");
 	basedb_savepoint(db);
-	int res = basedb_exec(db, "CREATE TABLE foo (bar INTEGER)");
+	int res = basedb_exec(db, "CREATE TABLE IF NOT EXISTS foo (bar INTEGER)");
 	ensure_eq(res, succeed);
 
 	printf("has table? %s\n", basedb_has_table(db, "foo") ? "ye" : "ne");
