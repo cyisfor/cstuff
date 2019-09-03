7275d796af1dd77b87cfc7f461684a2cfecdefd7
user <email>
2019-09-03 00:16:37 +0000

    better wording

--- test/test_basedb.c
+++ test/test_basedb.c
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
 	basedb db = basedb_open(.path = ":memory:");
 	basedb_savepoint(db);
 	int res = basedb_exec(db, "CREATE TABLE IF NOT EXISTS foo (bar INTEGER)");
-	ensure_eq(res, succeed);
+	ensure_eq(res, success);
 
 	printf("has table? %s\n", basedb_has_table(db, "foo") ? "ye" : "ne");
 	basedb_release(db);
